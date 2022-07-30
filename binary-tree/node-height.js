78014fd882e5e00369f6a775924a741fd404fbff
Hugo Deiró <hdeiro@192.168.1.18>
2022-07-30 09:52:20 -0300

    Rename tree-height for node-height (makes more sense)

--- binary-tree/node-height.js
+++ binary-tree/node-height.js
@@ -2,17 +2,17 @@ import { getDefinedTree } from '../mocks/generate-tree';
 
 const treeStructure = getDefinedTree();
 
-export const findHeightOnTree = (node) => {
+export const findNodeHeight = (node) => {
     // When doesn't exists it's trying to fetch from node that doesn't exists (like a child of a leaf)
     if (!node) return -1; // It's -1 due to the addition of 1 in main return statement
 
     // Check the height for right/left
     const maxHeight = Math.max(
-        findHeightOnTree(node.left),
-        findHeightOnTree(node.right)
+        findNodeHeight(node.left),
+        findNodeHeight(node.right)
     );
 
     return maxHeight + 1;
 }
 
-console.log(findHeightOnTree(treeStructure.ROOT));
+console.log(findNodeHeight(treeStructure.ROOT));
