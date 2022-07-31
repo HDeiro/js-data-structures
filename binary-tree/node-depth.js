7a1f763a84b227f1efffd2f62a532e45c2850b89
Hugo Deiró <hdeiro@192.168.1.18>
2022-07-31 09:17:12 -0300

    Adding more details on tree functions

--- binary-tree/node-depth.js
+++ binary-tree/node-depth.js
@@ -2,11 +2,15 @@ import { getDefinedTree } from '../mocks/generate-tree';
 
 const treeStructure = getDefinedTree();
 
+/**
+ * The depth of a node is the longest upward path from the given
+ * node to the root node.
+ */
 export const findNodeDepth = (node) => {
     // If node doesn't exists, return -1 as a flag 
     if (!node) return -1;
 
-    // No more parent levels
+    // No more parent levels (It's root)
     if (!node.parent) return 0;
 
     // Get recursively how deep is the node
--- binary-tree/node-height.js
+++ binary-tree/node-height.js
@@ -2,6 +2,11 @@ import { getDefinedTree } from '../mocks/generate-tree';
 
 const treeStructure = getDefinedTree();
 
+/**
+ * The height of a node is the longest downward path from the node to a leaf node.
+ * 
+ * The height of a tree is the height of the root node.
+ */
 export const findNodeHeight = (node) => {
     // When doesn't exists it's trying to fetch from node that doesn't exists (like a child of a leaf)
     if (!node) return -1; // It's -1 due to the addition of 1 in main return statement
